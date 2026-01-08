41aa0eadb846a15124f671792e1e54170cd62159
mirkobechini <mirkobechini@gmail.com>
2026-01-08 14:53:18 +0100

    Style: Added layout in layouts/master.blade.php Added Header in partials/header.blade.php Added Footer in partials/footer.blade.php Added Style to table in index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,47 +8,44 @@
 </head>
 
 <body>
-    <div class="container">
+    @extends('layouts.master')
+    @section('content')
+        <div class="container">
+            <div class="table-responsive border border-2 border-primary rounded my-3">
+                <table class="table  table-striped">
+                    <thead>
+                        <tr class="bg-primary">
+                            <th scope="col" class="bg-primary-subtle">Azienda</th>
+                            <th scope="col" class="bg-primary-subtle">Stazione Partenza</th>
+                            <th scope="col" class="bg-primary-subtle">Stazione Arrivo</th>
+                            <th scope="col" class="bg-primary-subtle">Orario Partenza</th>
+                            <th scope="col" class="bg-primary-subtle">Orario Arrivo</th>
+                            <th scope="col" class="bg-primary-subtle">Codice Treno</th>
+                            <th scope="col" class="bg-primary-subtle">Totale Carrozze</th>
+                            <th scope="col" class="bg-primary-subtle">In orario</th>
+                            <th scope="col" class="bg-primary-subtle">Cancellato</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($trains as $train)
+                            <tr class="">
+                                <td scope="row">{{ $train['company'] }}</td>
+                                <td>{{ $train['start_station'] }}</td>
+                                <td>{{ $train['arrive_station'] }}</td>
+                                <td>{{ $train['start_dateTime'] }}</td>
+                                <td>{{ $train['arrive_dateTime'] }}</td>
+                                <td class="text-center">{{ $train['train_code'] }}</td>
+                                <td class="text-center">{{ $train['carriages_number'] }}</td>
+                                <td>{{ $train['onTime'] }}</td>
+                                <td>{{ $train['deleted'] }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
 
-        <h1>ciao</h1>
-
-        <div
-            class="table-responsive">
-            <table
-                class="table table-primary">
-                <thead>
-                    <tr>
-                        <th scope="col">Azienda</th>
-                        <th scope="col">Stazione Partenza</th>
-                        <th scope="col">Stazione Arrivo</th>
-                        <th scope="col">Orario Partenza</th>
-                        <th scope="col">Orario Arrivo</th>
-                        <th scope="col">Codice Treno</th>
-                        <th scope="col">Totale Carrozze</th>
-                        <th scope="col">In orario</th>
-                        <th scope="col">Cancellato</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($trains as $train)
-                    <tr class="">
-                        <td scope="row">{{$train["company"]}}</td>
-                        <td>{{$train["start_station"]}}</td>
-                        <td>{{$train["arrive_station"]}}</td>
-                        <td>{{$train["start_dateTime"]}}</td>
-                        <td>{{$train["arrive_dateTime"]}}</td>
-                        <td>{{$train["train_code"]}}</td>
-                        <td>{{$train["carriages_number"]}}</td>
-                        <td>{{$train["onTime"]}}</td>
-                        <td>{{$train["deleted"]}}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
         </div>
-
-    </div>
-
+    @endsection
 
 </body>
 
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header class="bg-primary-subtle">
+    <div class="container">
+        <nav class="navbar text-light">
+            <div class="container">
+                <h1 class="navbar-brand fw-bold" href="#">Treni</h1>
+            </div>
+        </nav>
+    </div>
+</header>
